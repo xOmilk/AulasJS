8c833ea66b3e4051aaf8c048be685f8bc0de0804
Antônio Leite <128553621+xOmilk@users.noreply.github.com>
2025-04-15 16:48:27 -0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+function eventoClick(){
+    /* alert("Acionou um evento de click "); */
+    if(document.body.style.backgroundColor == "yellow"){
+        document.body.style.backgroundColor = "blue";
+    }
+    else{
+        document.body.style.backgroundColor = "yellow";
+
+    }
+}
+
+
+
+function eventoDblClick(){
+    document.getElementById="botao1".style.backgroundColor="black";
+}
