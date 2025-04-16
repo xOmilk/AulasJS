64f49c3781f0e4be66fdb2dc1011217f39910ee3
xOmilk <leite46052@gmail.com>
2025-04-15 23:20:03 -0300

    eventos

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,65 @@
-function eventoClick(){
+function eventoClick() {
     /* alert("Acionou um evento de click "); */
-    if(document.body.style.backgroundColor == "yellow"){
+    if (document.body.style.backgroundColor == "red") {
         document.body.style.backgroundColor = "blue";
     }
-    else{
-        document.body.style.backgroundColor = "yellow";
+    else {
+        document.body.style.backgroundColor = "red";
 
     }
 }
 
 
 
-function eventoDblClick(){
-    document.getElementById="botao1".style.backgroundColor="black";
+function eventoDblClick() {
+    alert("VOCE CLICOU DUAS VEZES");
+    document.getElementById = "botao2".style.backgroundColor = "black";
+}
+
+
+function onMouseOver() {
+ /*    alert("ok"); */
+    let div=document.getElementById("teste");
+    div.style.backgroundColor = "red";
+
+    if(div.style.backgroundColor == "red"){
+        div.style.backgroundColor = "black";
+        div.style.border="solid 2px black";
+        div.style.backgroundColor="white";
+    }
+    
+}
+
+function adicionaTexto() {
+    let p=document.getElementById("texto");
+    p.append("O mouse moveu");
+}
+function onMouseUp(){
+
+}
+
+function focar(){
+    /* alert("focado"); */
+}
+
+function limpaTexto(){
+    document.getElementById("idCampoTexto").value= ""
+}
+
+function mudou(){
+    alert("MUDOU");
+}
+
+function teclaPressionada(){
+    let input= document.getElementById("novoInput").value;
+    alert(input);
+
+}
+
+function carregou(){
+    alert("A pagina terminou de carregar");
+}
+
+function resize(){
+    alert("Você redimensionou a pagina");
 }
